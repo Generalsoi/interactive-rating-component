b12815e25da1c36085dd1a8649bc1f10f677fdaa
Success <ikhinobelesuccess@gmail.com>
2022-07-19 04:18:47 +0100

    added a thank you image

--- src/components/thankyoucard/thankyou.jsx
+++ src/components/thankyoucard/thankyou.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./thankyou.css";
+import ThankYou from "../../assets/images/thankyouimg";
 
 const Thankyou = () => {
-  return <div>Thankyou</div>;
+  return (
+    <div className="card">
+      <div className="thankyou-img">
+        <img src={ThankYou} alt="thankyou" />
+      </div>
+    </div>
+  );
 };
 
 export default Thankyou;
